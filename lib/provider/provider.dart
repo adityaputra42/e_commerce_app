9ef3bddfb7e8e8aa44a23ceb38d95670031eb145
aditiyaputra42 <aditiyaputra42@gmail.com>
2022-07-27 22:35:52 +0700

    menambahkan register dan login

---
+++ lib/provider/provider.dart
@@ -0,0 +1,7 @@
+import 'package:e_commerce_app/models/model.dart';
+import 'package:e_commerce_app/services/services.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+part 'auth_provider.dart';
+part 'product_provider.dart';
